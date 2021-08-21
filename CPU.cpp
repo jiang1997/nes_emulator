338bdd768354a506404351b71975711ff62edfd5
Jieke Jiang <jieke@live.cn>
2021-08-22 01:49:41 +0800

    refine

--- CPU.cpp
+++ CPU.cpp
@@ -4,21 +4,35 @@
 #include <cstdint>
 #include <cstdio>
 
-class CPU {
+class CONTEXT {
 public:
     uint8_t register_a;
+    uint8_t register_x;
     uint8_t status;
     uint16_t program_counter;
+};
 
-    std::unordered_map<uint8_t, void (*)()> opcodeMap;
 
-    CPU() {
-        register_a = 0;
-        status = 0;
-        program_counter = 0;
+class OPCODE {
+public:
+    virtual void proceed(CONTEXT& ctx) {
+        std:printf("from parent\n");
+    }
+};
+
+
+class CPU {
+public:
+    CONTEXT ctx;
+    std::unordered_map<uint8_t, OPCODE*> opcodeMap;
+
+    CPU(): ctx() {
+        ctx.register_a = 0;
+        ctx.status = 0;
+        ctx.program_counter = 0;
     }
 
-    void register_opencode(uint8_t opcode, void (*fn)()) {
+    void register_opencode(uint8_t opcode, OPCODE* fn) {
         opcodeMap.emplace(opcode, fn);
     }
 
@@ -30,13 +44,17 @@ void  CPU::interpret(std::vector<uint8_t> progream) {
 
 }
 
-void f1() {
-    std::printf("Hello World");
-}
+class _ll: public OPCODE {
+public:
+    void proceed(CONTEXT& ctx) override {
+        std::printf("Hello\n");
+    }
+};
+
 
 int main() {
     CPU c1;
-    c1.register_opencode(12, f1);
-    // c1.opcodeMap[12]();
+    c1.register_opencode(12, new _ll());
+    (*c1.opcodeMap[12]).proceed(c1.ctx);
     return 0;
 }
